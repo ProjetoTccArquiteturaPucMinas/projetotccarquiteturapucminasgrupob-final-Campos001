f09ddaa91abd2c4a1f9dfcf457414c7cf2da8fb6
João Pedro Campos de Barcelos <97189508+Campos001@users.noreply.github.com>
2026-04-17 19:08:06 -0300

    Commit da ativadade

--- marketplace/src/main/java/com/seuprojeto/marketplace/domain/model/CategoriaProduto.java
+++ marketplace/src/main/java/com/seuprojeto/marketplace/domain/model/CategoriaProduto.java
@@ -1,9 +1,19 @@
 package com.seuprojeto.marketplace.domain.model;
 
 public enum CategoriaProduto {
-    CAPINHA,
-    CARREGADOR,
-    FONE,
-    PELICULA,
-    SUPORTE
+    CAPINHA(0.03),
+    CARREGADOR(0.05),
+    FONE(0.03),
+    PELICULA(0.02),
+    SUPORTE(0.02);
+
+    private final BigDecimal percentualDesconto;
+
+    CategoriaProduto(BigDecimal percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public BigDecimal getpercentualDesconto() {
+         return categoriaProduto.this.percentualDesconto;
+    }   
 }
--- marketplace/src/main/java/com/seuprojeto/marketplace/domain/model/ResumoCarrinho.java
+++ marketplace/src/main/java/com/seuprojeto/marketplace/domain/model/ResumoCarrinho.java
@@ -41,4 +41,46 @@ public class ResumoCarrinho {
             this.total = subtotal.subtract(desconto);
         }
     }
+
+    private void CalcularDesconto(List<Produto> produtos) {
+        BigDecimal descontoTotal = new BigDecimal(0);
+
+        for(int i = 0; i < produtos.size(); i++) {
+            BigDecimal descontoPorCategoria = CalcularDescontoPorCategoria(produtos.get(i));
+            descontoTotal = descontoTotal.add(descontoPorCategoria);
+        }
+         descontoPorCategoria.CalcularDescontoPorQuantidade(descontoPorQuantidade);
+        if(descontoTotal >= 0.25){
+            descontoTotal = descontoTotal
+        }
+        else{
+            desconto = descontoTotal; 
+        }
+    }
+    private BigDecimal CalcularDescontoPorCategoria(Produto produto) {
+        return produto.getCategoriaProduto().getpercentualDesconto();
+    }
+    private BigDecimal CalcularDescontoPorQuantidade(List<Produto> produtos) {
+
+        switch (produtos.size()) {
+            case 0:
+                return new BigDecimal(0);
+            case 1:
+                return new BigDecimal(0);
+            case 2:
+                return new BigDecimal(0.05);
+            case 3:
+                return new BigDecimal(0.07);
+            default:
+                return new BigDecimal(0.1);
+        }
+    }
+    private void CalcularTotal(){
+        if(this.desconto <= 0 || this.desconto != null && this.subtotal >= 0 || this.subtotal != null){
+           this.total = this.subtotal * this.desconto;
+        }
+        else{
+            return this.total;
+        }
+    }
 }
